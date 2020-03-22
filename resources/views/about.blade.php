b8ab6fc8bbed4dc23ff07f50c46a822ff1f645ab
LDK <doomymax@gmail.com>
2020-03-22 23:28:15 +0300

    add info team

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,7 @@
 <h1>О блоге</h1>
 <p>Эксперименты с Laravel на Хекслете</p>
+<ul>
+@foreach ($team as $member)
+    <li>{{$member['name']}}</li>
+@endforeach
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+$team = [
+    ['name' => 'Hodor', 'position' => 'programmer'],
+    ['name' => 'Joker', 'position' => 'CEO'],
+    ['name' => 'Elvis', 'position' => 'CTO'],
+];  
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function() {
-    return view('about');
+Route::get('/about', function() use ($team) {
+    return view('about', ['team' => $team]);
 });
