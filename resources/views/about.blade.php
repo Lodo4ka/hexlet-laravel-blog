13d291a563896ee4ce93904494ce6118881d5f5b
maxim.novikov <maxim.novikov@onlinesup.com>
2020-04-02 13:25:38 +0300

    create layout app

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,17 @@
-<h1>О блоге</h1>
-<p>Эксперименты с Laravel на Хекслете</p>
-<ul>
-@foreach ($team as $member)
-    <li>{{$member['name']}}</li>
-@endforeach
-</ul>
+@extends('layouts.app')
+
+@section('header')
+  <h1>О блоге</h1>
+@endsection
+
+@section('title')
+  Эксперименты с Laravel на Хекслете
+@endsection
+
+@section('content')
+  <ul>
+  @foreach ($team as $member)
+      <li>{{$member['name']}}</li>
+  @endforeach
+  </ul>
+@endsection
