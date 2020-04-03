03f27914954aabfe5edeecc25480bc3acddd16ea
Maxim Novikov <maxim.novikov@onlinesup.com>
2020-04-03 18:28:40 +0300

    add controller PageController

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-$team = [
-    ['name' => 'Hodor', 'position' => 'programmer'],
-    ['name' => 'Joker', 'position' => 'CEO'],
-    ['name' => 'Elvis', 'position' => 'CTO'],
-];  
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function() use ($team) {
-    return view('about', ['team' => $team]);
-});
+Route::get('/about', 'PageController@about');
